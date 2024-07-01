e5c039f2e698a79236e89291eece68f1df0605cd
Pablo Terradillos <tehsis@gmail.com>
2024-06-30 23:41:57 -0300

    Sending prompts from chat

--- app/routes/chat.tsx
+++ app/routes/chat.tsx
@@ -1,17 +1,29 @@
-import { LoaderFunctionArgs, defer, json } from "@remix-run/node";
-import { Await, Form, useLoaderData } from "@remix-run/react";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  defer,
+  json,
+} from "@remix-run/node";
+import {
+  Await,
+  Form,
+  useActionData,
+  useLoaderData,
+  useNavigate,
+  useNavigation,
+} from "@remix-run/react";
 import { Suspense, useEffect, useRef, useState } from "react";
 import { chat } from "~/models/llm.server";
 import { useEventSource } from "remix-utils/sse/react";
+import { emitter } from "~/models/emitter.server";
 
 export function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   return json({ prompt: url.searchParams.get("prompt") });
 }
 
-function usePrompt(prompt: string | null) {
-  if (!prompt) return "";
-  const message = useEventSource(`/sse/chat?prompt=${prompt}`, {
+function usePrompt() {
+  const message = useEventSource(`/sse/subscribe`, {
     event: "message",
   });
 
@@ -26,14 +38,52 @@ function usePrompt(prompt: string | null) {
   return messageComplete;
 }
 
-export default function ChatRoute() {
-  const { prompt } = useLoaderData<typeof loader>();
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+  const message = body.get("message");
+  emitter.emit("chat", message);
+  return json({ ok: true });
+}
 
-  const message = usePrompt(prompt);
+export default function ChatRoute() {
+  const message = usePrompt();
+  const navigation = useNavigation();
+  const actionData = useActionData<typeof action>();
+
+  const formRef = useRef<HTMLFormElement>(null);
+
+  useEffect(
+    function resetFormOnSuccess() {
+      if (navigation.state === "idle" && actionData?.ok) {
+        formRef.current?.reset();
+      }
+    },
+    [navigation.state, actionData],
+  );
 
   return (
-    <div>
-      <div>{message}</div>
+    <div className="bg-purple-950 w-full text-purple-50 h-full">
+      <div className="p-20">
+        {message != "" ? (
+          <div className="bg-purple-800 rounded p-2 mb-4 w-1/2">{message}</div>
+        ) : null}
+        <Form
+          ref={formRef}
+          className="bg-purple-900 rounded p-4 w-full"
+          method="POST"
+        >
+          <input
+            placeholder="Type a message..."
+            className="appearance-none placeholder:text-purple-300 bg-purple-500 p-2 rounded w-1/2"
+            type="text"
+            id="message"
+            name="message"
+          />
+          <button className="bg-purple-800 p-2 ml-2 rounded" type="submit">
+            Send
+          </button>
+        </Form>
+      </div>
     </div>
   );
 }
--- app/routes/sse.subscribe.ts
+++ app/routes/sse.subscribe.ts
@@ -1,25 +1,25 @@
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { eventStream } from "remix-utils/sse/server";
 import { interval } from "remix-utils/timers";
+import { emitter } from "~/models/emitter.server";
 import { chatStream } from "~/models/llm.server";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const prompt = url.searchParams.get("prompt") || "";
 
-  return eventStream(request.signal, function setup(send, abort) {
-    async function run() {
-      console.log("Querying the llm...");
-      console.log("Prompt: ", prompt);
+  return eventStream(request.signal, function setup(send) {
+    async function handle(prompt: string) {
       const res = await chatStream(prompt);
-      console.log("Querying completed... streaming");
       for await (const chunk of res) {
         send({ data: chunk, event: "message" });
       }
     }
 
-    run();
+    emitter.on("chat", handle);
 
-    return () => {};
+    return function clear() {
+      emitter.off("chat", handle);
+    };
   });
 }
